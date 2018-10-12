451f8c61f1f4945d40446e5546419bf356d3f051
voddan <dgvodopyan@gmail.com>
2018-10-12 14:16:22 +0300

    Added Roulette game client

--- src/main/kotlin/client/NaiveClient.kt
+++ src/main/kotlin/client/NaiveClient.kt
@@ -15,3 +15,26 @@ fun main(args: Array<String>) {
     val user = s.next()
     println(user)
 }
+
+object RouletteGameClient {
+
+    @JvmStatic fun main(args: Array<String>) {
+
+        println("Welcome to play Roulette!")
+        println("Do you bet ODD (1) or EVEN (2)? ")
+
+        val guess = input.nextInt()
+
+        val value = get("http://127.0.0.1:8080/random/slow").content.toString(Charsets.UTF_8).toInt()
+
+        println("Roulette landed on $value")
+
+        if(value % 2 == guess % 2)
+            println("Congrats, your guess $guess was correct! ")
+        else
+            println("Sorry, your guess $guess was incorrect :( ")
+
+    }
+
+    val input = Scanner(System.`in`);
+}
