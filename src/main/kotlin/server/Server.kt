ba24419a767f64f4a27976c7ec1300f54404d222
voddan <dgvodopyan@gmail.com>
2018-10-15 15:15:20 +0300

    Trying to cut corners for delays and error emulation

--- src/main/kotlin/server/Server.kt
+++ src/main/kotlin/server/Server.kt
@@ -1,11 +1,9 @@
 package server
 
+import io.ktor.application.ApplicationCallPipeline
 import io.ktor.application.call
 import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpMethod
-import io.ktor.http.HttpStatusCode
 import io.ktor.request.receiveText
-import io.ktor.response.respond
 import io.ktor.response.respondRedirect
 import io.ktor.response.respondText
 import io.ktor.routing.*
@@ -27,71 +25,109 @@ object Server {
         val server = embeddedServer(Netty, port = 8080) {
             routing {
                 route("/random") {
-
-                    get {
-                        call.respondText(random.nextInt(10000000).toString())
-                    }
-
-                    route("/slow") {
-
+                    delaysAndErrors {
                         get {
-                            delay(1000)
                             call.respondText(random.nextInt(10000000).toString())
-                        }
-
-                        get("/error") {
-                            delay(1000)
-                            if(random.nextBoolean())
-                                call.respondText(random.nextInt(10000000).toString())
-                            else
-                                error("Something went terribly wrong :(")
                         }
                     }
                 }
 
                 route("/sum") {
-                    get() {
-                        call.respondText(sum.toString())
+                    delaysAndErrors {
+                        get() {
+                            call.respondText(sum.toString())
+                        }
+
+                        post() {
+                            val text = call.receiveText()
+                            val value = text.toIntOrNull() ?: error("Can't parse $text to Int")
+
+                            val oldSum = sum.get()
+                            val newSum = oldSum + value
+                            val sign = if(value >= 0) "+" else "-"
+
+                            call.respondText("$sum $sign ${value.absoluteValue} = $newSum")
+                            sum.addAndGet(value)
+                        }
                     }
-
-                    post() {
-                        val text = call.receiveText()
-                        val value = text.toIntOrNull() ?: error("Can't parse $text to Int")
-
-                        val oldSum = sum.get()
-                        val newSum = oldSum + value
-                        val sign = if(value >= 0) "+" else "-"
-
-                        call.respondText("$sum $sign ${value.absoluteValue} = $newSum")
-                        sum.addAndGet(value)
-                    }
-                }
-
-                route("/sum/slow") {
-                    get() {
-                        delay(1000)
-                        call.respondText(sum.toString())
-                    }
-
-                    post() {
-                        delay(1000)
-
-                        val text = call.receiveText()
-                        val value = text.toIntOrNull() ?: error("Can't parse $text to Int")
-
-                        val oldSum = sum.get()
-                        val newSum = oldSum + value
-                        val sign = if(value >= 0) "+" else "-"
-
-                        call.respondText("$sum $sign ${value.absoluteValue} = $newSum")
-                        sum.addAndGet(value)
-                    }
-
-                    delaysAndErrors()
                 }
             }
         }
         server.start(wait = true)
     }
 
+}
+
+
+@ContextDsl
+fun Route.slow(path: String = "slow",
+               timeMillis: Long = 1000,
+               build: Route.() -> Unit): Route {
+
+    val slowRoute = this.createChild(object : RouteSelector(1.0) {
+        override fun evaluate(context: RoutingResolveContext, segmentIndex: Int): RouteSelectorEvaluation =
+            RouteSelectorEvaluation.Constant
+    })
+
+    slowRoute.intercept(ApplicationCallPipeline.Features) {
+        delay(timeMillis)
+        proceed()
+    }
+
+    slowRoute.build()
+
+    return slowRoute
+}
+
+@ContextDsl
+fun Route.error(path: String = "error",
+                build: Route.() -> Unit): Route {
+
+    val erroredRoute = this.createChild(object : RouteSelector(1.0) {
+        override fun evaluate(context: RoutingResolveContext, segmentIndex: Int): RouteSelectorEvaluation =
+            RouteSelectorEvaluation.Constant
+    })
+
+    erroredRoute.intercept(ApplicationCallPipeline.Features) {
+        if(Random().nextBoolean())
+            error("Request fails with 50% probability")
+        proceed()
+    }
+
+    erroredRoute.build()
+
+    return erroredRoute
+}
+
+@ContextDsl
+fun Route.delaysAndErrors(build: Route.() -> Unit): Route {
+
+    val route = this.createChild(object : RouteSelector(1.0) {
+        override fun evaluate(context: RoutingResolveContext, segmentIndex: Int): RouteSelectorEvaluation =
+            RouteSelectorEvaluation.Constant
+    })
+
+    route {
+        build()
+
+        route("slow") {
+            slow {
+                build()
+
+                route("error") {
+                    error {
+                        build()
+                    }
+                }
+            }
+        }
+
+        route("error") {
+            error {
+                build()
+            }
+        }
+    }
+
+    return route
 }
